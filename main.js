4e9ff12b15e67ab0c8bfeeee36fb4327ba30dde0
newtonkim <knewts98@gmail.com>
2022-10-26 14:44:44 +0300

    list rendering

--- main.js
+++ main.js
@@ -6,7 +6,13 @@
                 image: './assets/images/socks_green.jpg',
                 // inStock: true
                 inventory: 10,
-                onSale: true
+                onSale: true,
+                details: ['50% cotton', '30% wool', '20% polyester'],
+                variants: [
+                    { id: 2234, color: 'green' },
+                    {id: 2235, color: 'blue'}
+                ],
+                sizes: ['size 4', 'size 3',' size 5', 'size 6']
             }
         }
     }).mount('#app')
