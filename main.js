1b5ebaed477e0a97ae0cf7d6f5f1359276d36a8b
newtonkim <knewts98@gmail.com>
2022-10-26 15:22:01 +0300

    event listening

--- main.js
+++ main.js
@@ -2,8 +2,28 @@ const app = Vue.createApp({
 
     data() {
         return {
-            'Product': 'Socks',
-            'Description' : '100% Cotton'
+            cart:0,
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+            ]
+        }
+    }, 
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+
+        updateImage(event) {
+            this.image = event
+        },
+
+        removeFromCart() {
+            this.cart -= 1;
         }
     }
 }).mount('#app')
